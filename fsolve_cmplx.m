4b92b0b381f08d626d72deee4b6fc902cce16a09
MeganRSullivan <56322448+MeganRSullivan@users.noreply.github.com>
2020-12-16 15:23:13 -0500

    add fsolve_cmplx

---
+++ fsolve_cmplx.m
@@ -0,0 +1,17 @@
+function z = fsolve_cmplx(f,z0)
+% z = fsolve_cmplx(f,z0);
+% wrapper function for matlab's fsolve function; finds the real
+% and imaginary part of the root of a function whose argument has
+% an imaginary part of O(eps^3)
+% inputs
+%      f:  function handle
+%      z0: initial guess at the root
+options = optimoptions('fsolve','Display','none');
+    i = sqrt(-1);
+    x = real(z0);
+    y = imag(z0);
+    f2 = @(x,y) [real(f(x+i*y));
+          imag(f(x+i*y))/eps^3]; % we need to rescale by 1/eps^3 otherwise the imaginary part is too small
+    r = fsolve(@(r) f2(r(1),r(2)),[x;y],options);
+    z = r(1)+i*r(2);
+end
