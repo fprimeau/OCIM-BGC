f67ada1f2bbcd976082392c962872fe681bc72af
Megan Sullivan <56322448+MeganRSullivan@users.noreply.github.com>
2024-05-13 13:49:47 -0700

    (LOOK) ResetPar: add cell; add ibad, scale Tz, c2p test
    - ResetPar: add Cell Model Q10Photo
    
    - (LOOK) ResetPar: add ibad list to output
    |  - to keep track of which parameters get reset
    
    - (LOOK) ResetPar: change test cc&dd to test full c2p function
    
    - ResetPar: add C2P Tz model params ccT ddT
    
    - (LOOK) ResetPar: remove rescale Tz*1e8
    |  - bug leftover from changing how Tz is saved in par.
    |    used to be multiplied by 1e-8 during SetUp, so conversion
    |    here was necessary. No longer needed now that the rescale
    |    has been removed from SetUp. Tz in par is already scaled from 0 to 1
    
    - ResetPar: add cell model param kST0
    |  - skip some cell model parameters because the initial guess is
    |    poorly constrained, and big jumps in these parameter values
    |    won't cause the BGC model to break

--- Code_withCellModel/src/ResetPar.m
+++ Code_withCellModel/src/ResetPar.m
@@ -1,4 +1,4 @@
-function  x = ResetPar(x, par);
+function  [x, ibad] = ResetPar(x, par);
     global iter
     on = true; off = false;
     load(par.fxhat,'x0')
@@ -10,6 +10,10 @@ function  x = ResetPar(x, par);
     fprintf('current fb is %3.3e \n', fb)
     % parameter values
     pindx = par.pindx  ;
+
+    % when the suggested parameter value is outside the range of possible values, 
+    % add the parameter's indx (par.pindx) to a vector 
+    ibad =[];
     
     if (par.opt_sigP == on)
         isigP = pindx.lsigP      ;
@@ -17,6 +21,7 @@ function  x = ResetPar(x, par);
         xold   = exp(x0(isigP))   ;
         if (xnew > 1-par.gamma )
             x(isigP) = log( exp(x0(isigP)) + 0.1*rand ) ;
+            ibad = [ibad, isigP];
         end
     end
 
@@ -31,6 +36,7 @@ function  x = ResetPar(x, par);
         mkP0 = mean(kP0)  ; 
         if mkP1 > fb*mkP0 | mkP1 < fs*mkP0
             x(iQ10P) = log( exp(x0(iQ10P)) * (0.1*rand + 0.95) ) ;
+            ibad = [ibad, iQ10P];
         end
     end
 
@@ -50,6 +56,7 @@ function  x = ResetPar(x, par);
         if mkP1 > fb*mkP0 | mkP1 < fs*mkP0
             x(iQ10P) = log( exp(x0(iQ10P))*(0.1*rand + 0.95) );
             x(ikdP)  = log( exp(x0(ikdP))*(0.1*rand + 0.95) );
+            ibad = [ibad, iQ10P, ikdP];
         end
     end
     
@@ -59,6 +66,7 @@ function  x = ResetPar(x, par);
         xold = exp(x0(ikdP))  ;
         if (xnew > fb*xold | xnew < fs*xold);
             x(ikdP) = log( exp(x0(ikdP))*(0.1*rand + 0.95) );
+            ibad = [ibad, ikdP];
         end
     end
 
@@ -70,6 +78,7 @@ function  x = ResetPar(x, par);
             bC   = bm1*par.aveT + par.bC ;
             if mean(bC(:)) < 0.3 | mean(bC(:)) > 3
                 x(ibP_T) = x0(ibP_T) + (0.02*rand - 0.01);
+                ibad = [ibad, ibP_T];
             end
     end
     
@@ -86,6 +95,7 @@ function  x = ResetPar(x, par);
         if nanmean(bP(:)) < 0.3 | nanmean(bP(:)) > 3
             x(ibP_T) = x0(ibP_T)  + (0.02*rand - 0.01);
             x(ibP)   = log( exp(x0(ibP))*(0.1*rand + 0.95) );
+            ibad = [ibad, ibP_T, ibP];
         end
     end
     
@@ -95,6 +105,7 @@ function  x = ResetPar(x, par);
         xold = exp(x0(ibP)) ;
         if (xnew < 0.3 | xnew > 3) ;
             x(ibP) = log( exp(x0(ibP))*(0.1*rand + 0.95) );
+            ibad = [ibad, ibP];
         end
     end
         
@@ -104,6 +115,7 @@ function  x = ResetPar(x, par);
         xold   = exp(x0(ialpha)) ;
         if (xnew > fb*xold | xnew < fs*xold);
             x(ialpha) = log( exp(x0(ialpha))*(0.1*rand + 0.95) );
+            ibad = [ibad, ialpha];
         end
     end
 
@@ -113,6 +125,7 @@ function  x = ResetPar(x, par);
         xold  = exp(x0(ibeta)) ;
         if (xnew > fb*xold | xnew < fs*xold);
             x(ibeta) = log( exp(x0(ibeta))*(0.1*rand + 0.95) );
+            ibad = [ibad, ibeta];
         end
     end
 
@@ -123,6 +136,7 @@ function  x = ResetPar(x, par);
             xold   = exp(x0(isigC))   ;
             if (xnew > 1-par.gamma )
                 x(isigC) = log( exp(x0(isigC)) + 0.1*rand ) ;
+                ibad = [ibad, isigC];
             end
         end
 
@@ -132,6 +146,7 @@ function  x = ResetPar(x, par);
             xold   = exp(x0(ikru)) ;
             if (xnew > fb*xold | xnew < fs*xold);
                 x(ikru) = log( exp(x0(ikru))*(0.1*rand + 0.95) );
+                ibad = [ibad, ikru];
             end
         end
 
@@ -141,6 +156,7 @@ function  x = ResetPar(x, par);
             xold   = exp(x0(ikrd)) ;
             if (xnew > fb*xold | xnew < fs*xold);
                 x(ikrd) = log( exp(x0(ikrd))*(0.1*rand + 0.95) );
+                ibad = [ibad, ikrd];
             end
         end
 
@@ -150,6 +166,7 @@ function  x = ResetPar(x, par);
             xold   = exp(x0(ietau)) ;
             if (xnew > 1);
                 x(ietau) = log( xold ) ;
+                ibad = [ibad, ietau];
             end
         end
 
@@ -159,6 +176,7 @@ function  x = ResetPar(x, par);
             xold   = exp(x0(ietad)) ;
             if ( xnew > 1 )
                 x(ietad) = log( xold ) ;
+                ibad = [ibad, ietad];
             end
         end
 
@@ -170,6 +188,7 @@ function  x = ResetPar(x, par);
             bC   = bm1*par.aveT + par.bC ;
             if min(bC(:)) < 0.3 | max(bC(:)) > 3
                 x(ibC_T) = x0(ibC_T) + (0.02*rand - 0.01);
+                ibad = [ibad, ibC_T];
             end
         end
 
@@ -186,6 +205,7 @@ function  x = ResetPar(x, par);
             if min(bC(:)) < 0.3 | max(bC(:)) > 3
                 x(ibC_T) = x0(ibC_T)  + (0.02*rand - 0.01);
                 x(ibC)   = log( exp(x0(ibC))*(0.1*rand + 0.95) );
+                ibad = [ibad, ibC_T, ibC];
             end
         end
         
@@ -195,6 +215,7 @@ function  x = ResetPar(x, par);
             xold = exp(x0(ibC)) ;
             if (xnew < 0.3 | xnew > 3) ;
                 x(ibC) = log( exp(x0(ibC))*(0.1*rand + 0.95) );
+                ibad = [ibad, ibC];
             end
         end
 
@@ -204,6 +225,7 @@ function  x = ResetPar(x, par);
             xold = exp(x0(id)) ;
             if (xnew > fb*xold | xnew < fs*xold) ;
                 x(id) = log( exp(x0(id))*(0.1*rand + 0.95) );
+                ibad = [ibad, id];
             end
         end
 
@@ -218,6 +240,7 @@ function  x = ResetPar(x, par);
             mkC0 = mean(kC0) ; 
             if any(kC1 < 0) | mkC1 > fb*mkC0 | mkC1 < fs*mkC0
                 x(iQ10C) = log( exp(x0(iQ10C))*(0.1*rand + 0.95) );
+                ibad = [ibad, iQ10C];
             end
         end
 
@@ -237,6 +260,7 @@ function  x = ResetPar(x, par);
             if any(kC1 < 0) | mkC1 > fb*mkC0 | mkC1 < fs*mkC0
                 x(iQ10C) = log( exp(x0(iQ10C))*(0.1*rand + 0.95) );
                 x(ikdC)  = log( exp(x0(ikdC))*(0.1*rand + 0.95) );
+                ibad = [ibad, iQ10C, ikdC];
             end
         end
 
@@ -246,6 +270,7 @@ function  x = ResetPar(x, par);
             xold = exp(x0(ikdC)) ;
             if (xnew > fb*xold | xnew < fs*xold) ;
                 x(ikdC) = log( exp(x0(ikdC))*(0.1*rand + 0.95) );
+                ibad = [ibad, ikdC];
             end
         end
         
@@ -268,6 +293,7 @@ function  x = ResetPar(x, par);
             if (nRR > fb*oRR | nRR < fs*oRR);
                 x(iR_Si) = x0(iR_Si)  + (0.02*rand - 0.01);
                 x(irR)   = log( exp(x0(irR))*(0.1*rand + 0.95) );
+                ibad = [ibad, iR_Si, irR];
             end
         end
         
@@ -286,6 +312,7 @@ function  x = ResetPar(x, par);
             
             if (nRR > fb*oRR | nRR < fs*oRR);
                 x(iR_Si) = x0(iR_Si) + (0.02*rand - 0.01);
+                ibad = [ibad, iR_Si];
             end
         end
 
@@ -295,24 +322,78 @@ function  x = ResetPar(x, par);
             xold = exp(x0(irR)) ;
             if (xnew > fb*xold  | xnew < fs*xold);
                 x(irR) = log( exp(x0(irR))*(0.1*rand + 0.95) );
+                ibad = [ibad, irR];
             end
         end
         
-        if (par.opt_cc == on)
+        if (par.opt_cc == on & par.opt_dd == off)
             icc  = pindx.lcc    ;
             xnew = exp(x(icc))  ;
             xold = exp(x0(icc)) ;
             if (xnew > fb*xold  | xnew < fs*xold);
                 x(icc) = log( exp(x0(icc))*(0.1*rand + 0.95) );
+                ibad = [ibad, icc];
             end
         end
         
-        if (par.opt_dd == on)
+        if (par.opt_dd == on & par.opt_cc == off)
             idd  = pindx.ldd    ;
             xnew = exp(x(idd))  ;
             xold = exp(x0(idd)) ;
             if (xnew > fb*xold  | xnew < fs*xold);
                 x(idd) = log( exp(x0(idd)) * (0.1*rand + 0.95) );
+                ibad = [ibad, idd];
+            end
+        end
+
+        if (par.opt_cc == on & par.opt_dd == on)
+            icc = pindx.lcc ;
+			cc1 = exp(x(icc))  ;
+            cc0 = exp(x0(icc)) ;
+            %
+            idd  = pindx.ldd    ;
+            dd1  = exp(x(idd))  ;
+            dd0  = exp(x0(idd)) ;
+            % 
+			iprod = find(par.M3d(:,:,1:2));
+            c2p1 = 1./(cc1*par.po4obs(iprod) + dd1) ;
+			c2p0 = 1./(cc0*par.po4obs(iprod) + dd0) ;
+            mc2p1 = mean(c2p1) ;
+            mc2p0 = mean(c2p0) ;
+            if any(c2p1 < 0) %| mc2p1 > fb*mc2p0 | mc2p1 < fs*mc2p0
+				x(icc) = log( exp(x0(icc))*(0.1*rand + 0.95) );
+                x(idd)  = log( exp(x0(idd))*(0.1*rand + 0.95) );
+                ibad = [ibad, icc, idd];
+            end
+        end
+
+        if (par.opt_ccT == off & par.opt_ddT == on)
+            iddT  = pindx.lddT    ;
+            xnew = exp(x(iddT))  ;
+            xold = exp(x0(iddT)) ;
+            if (xnew > fb*xold  | xnew < fs*xold);
+                x(iddT) = log( exp(x0(iddT)) * (0.1*rand + 0.95) );
+                ibad = [ibad, iddT];
+            end
+        end
+
+		if (par.opt_ccT == on & par.opt_ddT == on)
+            iccT = pindx.ccT ;
+            ccT1 = x(iccT)   ;
+            ccT0 = x0(iccT)  ;
+            %
+            iddT  = pindx.lddT    ;
+            ddT1  = exp(x(iddT))  ;
+            ddT0  = exp(x0(iddT)) ;
+            %
+            c2p1 = 1./(ccT1*par.Tz + ddT1) ;
+			c2p0 = 1./(ccT0*par.Tz + ddT0) ;
+            mc2p1 = mean(c2p1) ;
+            mc2p0 = mean(c2p0) ;
+            if any(c2p1 < 0) 
+                x(iddT) = log( exp(x0(iddT)) * (0.1*rand + 0.95) );
+                x(iccT) = x0(iccT) + (0.02*rand - 0.01);
+				ibad = [ibad, iccT, iddT];
             end
         end
     end 
@@ -326,6 +407,7 @@ function  x = ResetPar(x, par);
             O2C = O2C_T1*par.Tz + par.rO2C1 ;
             if (min(O2C) < 0)    ;
                 x(iO2C_T) = x0(iO2C_T) + (0.02*rand - 0.01);
+                ibad = [ibad, iO2C_T];
             end
         end
 
@@ -338,11 +420,12 @@ function  x = ResetPar(x, par);
             rO2C1 = exp(x(irO2C)) ;
             rO2C0 = exp(x0(irO2C));
             %
-            O2C1 = O2C_T1*par.Tz*1e8 + rO2C1 ;
-            O2C0 = O2C_T0*par.Tz*1e8 + rO2C0 ;
+            O2C1 = O2C_T1*par.Tz + rO2C1 ;
+            O2C0 = O2C_T0*par.Tz + rO2C0 ;
             if (mean(O2C1) < fs*mean(O2C0) | mean(O2C1) > fb*mean(O2C0)) 
                 x(irO2C)  = log( exp(x0(irO2C))*(0.1*rand + 0.95) );
                 x(iO2C_T) = x0(iO2C_T) + (0.02*rand - 0.01);
+                ibad = [ibad, iO2C_T, iO2C];
             end
         end
 
@@ -352,6 +435,7 @@ function  x = ResetPar(x, par);
             xold  = exp(x0(irO2C)) ;
             if (xnew > fb*xold | xnew < fs*xold);
                 x(irO2C) = log( exp(x0(irO2C))*(0.1*rand + 0.95) );
+                ibad = [ibad, iO2C];
             end
         end
     end
@@ -364,6 +448,7 @@ function  x = ResetPar(x, par);
             xold = exp(x0(idsi)) ;
             if (xnew > 5000 | xnew < 1000);
                 x(idsi) = x0(idsi);
+                ibad = [ibad, idsi];
             end
         end
         % at
@@ -373,6 +458,7 @@ function  x = ResetPar(x, par);
             xold = exp(x0(iat)) ;
             if (xnew > fb*xold  | xnew < fs*xold);
                 x(iat) = x0(iat);
+                ibad = [ibad, iat];
             end
         end
         % bt
@@ -382,6 +468,7 @@ function  x = ResetPar(x, par);
             xold = exp(x0(ibt))  ;
             if (xnew > 15000     | xnew < 10000);
                 x(ibt) = x0(ibt) ;
+                ibad = [ibad, ibt];
             end
         end
         % aa
@@ -391,6 +478,7 @@ function  x = ResetPar(x, par);
             xold = x0(iaa)    ;
             if (xnew > 50 | xnew < -50);
                 x(iaa) = x0(iaa);
+                ibad = [ibad, iaa];
             end
         end
         % bb
@@ -400,9 +488,39 @@ function  x = ResetPar(x, par);
             xold = exp(x0(ibb)) ;
             if (xnew > fb*xold  | xnew < fs*xold);
                 x(ibb) = x0(ibb);
+                ibad = [ibad, ibb];
             end
         end    
     end
+    %------------------------
+	if (par.Cellmodel==on)
+        % resetting is probably not necessary for cell model parameters.
+        % C2P is always defined and positive, even for strange parameters.
+        % Thus, bad cell model parameters won't crash the BGC model.
+		% Q10Photo
+        if (par.opt_Q10Photo == on)
+	        iQ10Photo = pindx.lQ10Photo    ;
+	        xnew  = exp(x(iQ10Photo))  ;
+	        xold  = exp(x0(iQ10Photo)) ;
+	        if (xnew > fb*xold | xnew < fs*xold);
+	            x(iQ10Photo) = log( exp(x0(iQ10Photo))*(0.1*rand + 0.95) );
+                ibad = [ibad, iQ10Photo];
+	        end
+        end
+        % fStorage - leave unconstrained
+        % kST0
+        if (par.opt_kST0 == on)
+	        ikST0 = pindx.lkST0    ;
+	        xnew  = exp(x(ikST0))  ;
+	        xold  = exp(x0(ikST0)) ;
+	        if (xnew > fb*xold | xnew < fs*xold);
+	            x(ikST0) = log( exp(x0(ikST0))*(0.1*rand + 0.95) );
+                ibad = [ibad, ikST0];
+	        end
+        end
+        % PStor_rCutoff - leave unconstrained
+        % alphaS - leave unconstrained
+	end %end cell model
 
 end
 
