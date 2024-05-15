935967950495d71dd889509f66a16285e19bf04e
Megan Sullivan <56322448+MeganRSullivan@users.noreply.github.com>
2024-05-15 01:03:52 -0700

    driver: update paths, output & run options
    - add file header
    - change output filenames, add cell model option,
      include VerName and VerNum strings
    - change objective function tolerance
    - change GHtest output;
    - add option to run without optim
    - remove if optim to run SetPar & PackPar
    - set options for PCO_Cell_GHtest
    - add option for fxhatload
    - change output folder path

--- Code_withCellModel/driver/driver.m
+++ Code_withCellModel/driver/driver.m
@@ -1,14 +1,27 @@
-clc; clear all; close all 
+% driver_optC_Cell.m
+%   Driver script to run biogeochemical model.
+%   The carbon cycle uses a trait-based celllular growth model to compute C:P.
+%   This run optimizes phosphorus, carbon, and oxygen cycle parameters
+% ------------------------------------------------------------------------
+clc; clear all; close all
+global iter
+iter = 0 ;
+on   = true  ; off  = false ;
+format short
+
+% --- addpath to model code -----
 addpath('../src/')
 
-global iter 
-iter = 0 ;
-on   = true  ;
-off  = false ;
-format short
+VerName = 'testPCO_Cell_'; 		% optional version name. leave as an empty character array
+					% or add a name ending with an underscore
+VerNum = '';		% optional version number for testing
+
+% Choose C2P function
+par.C2Pfunctiontype = 'C';
+% 'P' -> PO4 function ; 'C' -> Cell model; 'T' -> Temperature function; 'R' -> constant value (Redfield)
 % 
 GridVer  = 91  ;
-operator = 'L' ;
+operator = 'A' ;
 % GridVer: choose from 90 and 91; Ver 90 is for a Transport
 % operator without diapycnal mixing but optimized using DIP ;
 % Ver 91 include a bunch of operators that include diapycnal
@@ -19,16 +32,19 @@ operator = 'L' ;
 % H -> KiLOW_noHe; I -> KvHIGH_He; J -> KvHIGH_noHe; K -> KvHIGH_KiHIGH_noHe
 % L -> CTL_He_48layer
 
-par.nl = 2;
+par.nl = 2; % number of layers in the model euphotic zone (doesn't change)
 
-Gtest = off ; 
-Htest = off ;
-par.optim   = on ; 
+Gtest = on ; 
+Htest = on ;
+par.optim   = off ; 
 par.Cmodel  = on ; 
 par.Omodel  = on ; 
 par.Simodel = off ;
-par.Cisotope  = on  ;
-par.LoadOpt = on ; % if load optimial par. 
+par.Cisotope  = off  ;
+par.LoadOpt = off ; % if load optimial parameters. 
+% to load parameter values from a run with a different name.
+par.fxhatload = '../../output/optPonly_CTL_He_P_xhat.mat';
+par.dynamicP = off ; % if on, cell model uses modeled DIP. if off, cell model uses WOA observed DIP field.
 
 par.dopscale = 0.0 ;
 par.dipscale = 1.0 ;
@@ -57,8 +73,24 @@ par.opt_Q10C  = on ;
 par.opt_kdC   = on ; 
 par.opt_R_Si  = on ; 
 par.opt_rR    = on ; 
-par.opt_cc    = on ;
-par.opt_dd    = on ;
+% --- C:P function parameters -----
+% phosphate-dependent function parameters
+par.opt_cc    = off ;
+par.opt_dd    = off ; 
+% temperature-dependent function parameters
+par.opt_ccT   = off; 
+par.opt_ddT   = off;
+% Trait-based Cellular Growth Model parameters
+par.opt_Q10Photo     = on ; % opt
+par.opt_fStorage     = on ; % opt
+par.opt_fRibE 	     = off ; 
+par.opt_kST0 	     = on ; % opt
+par.opt_PLip_PCutoff = off ;
+par.opt_PLip_scale   = off ;
+par.opt_PStor_rCutoff = on; % opt
+par.opt_PStor_scale  = off ;
+par.opt_alphaS       = on ; % opt
+par.opt_gammaDNA	 = off ;
 % O model parameters
 par.opt_O2C_T = off ;
 par.opt_rO2C  = on ;
@@ -73,47 +105,59 @@ par.opt_bb    = on  ;
 SetUp ;                      
 
 % save results 
-% ATTENTION: Change this direcrtory to where you wanna
+% ATTENTION: Change this directory to where you want to
 % save your output files
-output_dir = sprintf('../Results/'); 
+output_dir = sprintf('../output/'); 
 
 if ~isdir(output_dir)
     command = strcat('mkdir', " ", output_dir) ;
     system(command) ;
 end
 
-VER = strcat(output_dir,TRdivVer);
-% Creat output file names based on which model(s) is(are) optimized
-if Gtest == on
-    fname = strcat(VER,'_GHtest');
-elseif Gtest == off
-    if (par.Cmodel == off & par.Omodel == off & par.Simodel == off)
-        fname = strcat(VER,'_Pv3');
-    elseif (par.Cmodel == on & par.Omodel == off & par.Simodel == off)
-        base_name = strcat(VER,'_PCv1');
-        catDOC = sprintf('_DOC%2.0e_DOP%2.0e',par.cscale,par.pscale);
+VER = strcat(output_dir,VerName,TRdivVer);
+catDOC = sprintf('_DOC%0.2g_DOP%0.2g',par.docscale,par.dopscale); % used to add scale factors to file names
+% Create output file names based on which model(s) is(are) optimized
+%if Gtest == on
+%    fname = strcat(VER,'_GHtest');
+%elseif Gtest == off
+    if (par.Cmodel == off & par.Omodel == off & par.Simodel == off & par.Cellmodel == off)
+        fname = strcat(VER,'_P',VerNum);
+    elseif (par.Cmodel == on & par.Omodel == off & par.Simodel == off & par.Cellmodel == off)
+        base_name = strcat(VER,'_PC',VerNum);
         fname = strcat(base_name,catDOC);
-    elseif (par.Cmodel == on & par.Omodel == on & par.Simodel == off)
-        base_name = strcat(VER,'_PCO_firstrun');
-       % catDOC = sprintf('_DIP%2.0e_DIC%2.0e_DOC%2.0e_ALK%2.0e_O2%2.0e', ...
-                       %  par.dipscale,par.dicscale,par.docscale,par.alkscale,par.o2scale);
-        fname = strcat(base_name);
-       %fname = strcat(base_name,catDOC);
-    elseif (par.Cmodel == on & par.Omodel == off & par.Simodel == on)
-        base_name = strcat(VER,'_PCSi');
-        catDOC = sprintf('_DOC%2.0e_DOP%2.0e',par.cscale,par.pscale);
+    elseif (par.Cmodel == on & par.Omodel == on & par.Simodel == off & par.Cellmodel == off)
+        base_name = strcat(VER,'_PCO',VerNum);
         fname = strcat(base_name,catDOC);
-    elseif (par.Cmodel == on & par.Omodel == on & par.Simodel == on)
-        base_name = strcat(VER,'_PCOSi');
-        catDOC = sprintf('_DOC%2.0e_DOP%2.0e',par.cscale,par.pscale);
+    elseif (par.Cmodel == on & par.Omodel == off & par.Simodel == on & par.Cellmodel == off)
+        base_name = strcat(VER,'_PCSi',VerNum);
+        fname = strcat(base_name,catDOC);
+    elseif (par.Cmodel == on & par.Omodel == on & par.Simodel == on & par.Cellmodel == off)
+        base_name = strcat(VER,'_PCOSi',VerNum);
+        fname = strcat(base_name,catDOC);
+	elseif (par.Cmodel == off & par.Omodel == off & par.Simodel == off & par.Cellmodel == on) % cell model does nothing if C model is not on, so this case =Ponly
+        base_name = strcat(VER,'_PCell',VerNum);
+        fname = strcat(base_name,catDOC);
+	elseif (par.Cmodel == on & par.Omodel == off & par.Simodel == off & par.Cellmodel == on)
+        base_name = strcat(VER,'_PCCell',VerNum);
+        fname = strcat(base_name,catDOC);
+	elseif (par.Cmodel == on & par.Omodel == on & par.Simodel == off & par.Cellmodel == on)
+		base_name = strcat(VER,'_PCOCell',VerNum);
+		fname = strcat(base_name,catDOC);
+	elseif (par.Cmodel == on & par.Omodel == on & par.Simodel == on & par.Cellmodel == on)
+        base_name = strcat(VER,'_PCOSiCell',VerNum);
         fname = strcat(base_name,catDOC);
     end
-end
-% -------------------- Set up output files ---------------
-par.fname = strcat(fname,'.mat') ; 
-fxhat     = strcat(fname,'_xhat.mat');
-fxpar     = strcat(fname,'_par.mat');
+%end
 
+% -------------------- Set up output files ---------------
+% -------------------- Set up output files ---------------
+par.fname = strcat(fname,'.mat') ;
+fxhat     = strcat(fname,'_xhat.mat') 
+fxpar     = strcat(fname,'_par.mat');
+par.fxhat = fxhat ;
+if Htest ==on
+	fGHtest = strcat(fname,'_GHtest.mat')  ;
+end
 par.fxhat = fxhat ;
 par.fxpar = fxpar ;
 
@@ -133,16 +177,15 @@ if par.Omodel == on
 end 
 
 %--------------------- prepare parameters ------------------
-if par.optim == on 
-    % load optimal parameters from a file or set them to default values 
-    par = SetPar(par)  ;
-    % pack parameters into an array, assign them corresponding indices.
-    par = PackPar(par) ;
-end 
+% load optimal parameters from a file or set them to default values 
+par = SetPar(par)  ;
+% pack parameters into an array, assign them corresponding indices.
+par = PackPar(par) ;
 
 %-------------------set up fminunc -------------------------
 x0    = par.p0 ;
 myfun = @(x) neglogpost(x, par);
+objfuntolerance = 5e-11; %5e-12;
 options = optimoptions(@fminunc                  , ...
                        'Algorithm','trust-region', ...
                        'GradObj','on'            , ...
@@ -150,22 +193,32 @@ options = optimoptions(@fminunc                  , ...
                        'Display','iter'          , ...
                        'MaxFunEvals',2000        , ...
                        'MaxIter',2000            , ...
-                       'TolX',1e-10               , ...     % 기존은 -7. decreasing
-                       'TolFun',1e-10             , ...     % 기존은 -7. decreasing
+                       'TolX', objfuntolerance   , ...     % 기존은 -7. decreasing
+                       'TolFun',objfuntolerance  , ...     % 기존은 -7. decreasing
                        'DerivativeCheck','off'   , ...
                        'FinDiffType','central'   , ...
                        'PrecondBandWidth',Inf)   ;
 %
 nip = length(x0);
 if(Gtest);
+    fprintf('derivative test \n')
+	GHtest.fx_cstep = NaN([nip,1]);
+	GHtest.fxx_cstep = NaN(nip);
+	GHtest.pindx = par.pindx;
     dx = sqrt(-1)*eps.^3*eye(nip);
     for ii = 1 : nip 
         x  = real(x0)+dx(:,ii);
         if Htest == on
             [f,fx,fxx] = neglogpost(x, par) ;
+            GHtest.fx_cstep(ii) = imag(f)/eps.^3     ;
+			GHtest.fxx_cstep(:,ii) = imag(fx)/eps.^3 ;
+			%save complex step gradient and hessian
+			fprintf('saving GHtest to file: %s \n',fGHtest)
+			save(fGHtest,'GHtest')
+
             % print relative errors
             diff = (real(fx(ii)) - imag(f)/eps.^3)/(imag(f)/eps.^3);
-            fprintf('%i % .3e  \n',ii,diff);
+            fprintf('gradient relative error (%i): % .3e  \n',ii,diff);
             diffx = (real(fxx(:,ii))-imag(fx)/eps.^3)./(imag(fx)/eps.^3+eps.^3);
             for jj = 1:length(fx)
                 fprintf('% .3e  ', diffx(jj));
@@ -178,17 +231,33 @@ if(Gtest);
         end 
         fprintf('\n');                                                     % 이 부분을 바꾸던데..
     end
-    keyboard  
-else
-    [xhat,fval,exitflag] = fminunc(myfun,x0,options);
-    [f,fx,fxx,data] = neglogpost(xhat,par);
-    load(fxhat)
+    format shortE
+	real(fx)
+	real(full(fxx))
+    %keyboard  
+elseif (par.optim)
+    [xsol,fval,exitflag] = fminunc(myfun,x0,options);
+    fprintf('objective function tolerance = %5.1e \n',objfuntolerance);
+    fprintf('----fminunc complete----\n')
+    [f,fx,fxx,data,xhat] = neglogpost(xsol,par);
+    fprintf('----neglogpost solved for final parameter values----\n')
+    xhat.pindx = par.pindx;
     xhat.f   = f   ;
     xhat.fx  = fx  ;
     xhat.fxx = fxx ;
     % save results 
+    fprintf('saving optimized parameters to file: %s \n',fxhat)
+    fprintf('saving model solution to file: %s \n',par.fname)
     save(fxhat, 'xhat')
     save(par.fname, 'data')
+else
+    xsol = x0;
+    iter = 6;
+    [f,fx,fxx,data] = neglogpost(xsol,par);
+    fprintf('----neglogpost complete----\n')
+    %% note: skipping save for testing
+    %fprintf('saving model solution to file: %s \n',par.fname)
+    %save(par.fname, 'data')
 end
 fprintf('-------------- end! ---------------\n');
 quit
