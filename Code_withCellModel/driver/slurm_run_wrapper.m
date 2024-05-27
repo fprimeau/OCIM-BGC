bd45b908fb81b6575802752e1c2b3d3906521fa9
Megan Sullivan(gp) <56322448+MeganRSullivan@users.noreply.github.com>
2024-05-26 19:33:49 -0700

    add diary to write command line output to a file
    to save the output log to a plain text file.
    If running the model from inside the compute node directly (without slurm), the diary command in matlab should create a similar log file to the .out file created automatically for slurm jobs.

--- Code_withCellModel/driver/slurm_run_wrapper.m
+++ Code_withCellModel/driver/slurm_run_wrapper.m
@@ -2,4 +2,6 @@
 % running this script in the slurm matlab call instead of running driver.m
 % directly should suppress the command line prompts from appearing in the
 % output file
+diary logs/optPCOCell_v1continued.out
 run driver.m
+diary off
