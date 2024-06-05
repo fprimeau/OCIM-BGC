afbcce4d1f4ac0c723321a8c3990d79dc7561439
Megan Sullivan(gp) <56322448+MeganRSullivan@users.noreply.github.com>
2024-06-05 14:08:20 -0700

    tmp: start run reoptNature
    also added print datetime to slurm_run_wrapper

--- Code_withCellModel/driver/slurm_run_wrapper.m
+++ Code_withCellModel/driver/slurm_run_wrapper.m
@@ -2,9 +2,11 @@
 % running this script in the slurm matlab call instead of running driver.m
 % directly should suppress the command line prompts from appearing in the
 % output file
-diary logs/optPCO_Tz_v2.out
-fprintf('----------------------------------- \n\n')
-datetime('now')
-fprintf('continue optim after fixing gradient for ccT and ddT \n\n')
-run driver.m
+diary logs/reoptNature.out
+%fprintf('----------------------------------- \n\n')
+date_start = datetime('now')
+fprintf('Reoptimizing the Nature paper (Wang et al., 2023) using parameters from hojongs previous run and only BGC_2023Nature input data \n\n')
+run driver_reoptNature.m
+date_end = datetime('now')
+fprintf('Total elapsed time was %s\n',date_end - date_start)
 diary off
