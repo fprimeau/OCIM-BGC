01f01d48cc7da9f9216819af33c4f8482a9b7623
Megan Sullivan <56322448+MeganRSullivan@users.noreply.github.com>
2024-06-18 11:27:45 -0400

    tmp: start run optPCO_GM15_N23in_nosmooth

--- Code_withCellModel/driver/driver_SetUpv2.m
+++ Code_withCellModel/driver/driver_SetUpv2.m
@@ -12,12 +12,12 @@ format short
 % --- addpath to model code -----
 addpath('../src/')
 
-VerName = 'optPCO_Cell_SetUpv2_N23in_'; 		% optional version name. leave as an empty character array
+VerName = 'optPCO_GM15_SetUpv2_N23in_'; 		% optional version name. leave as an empty character array
 					% or add a name ending with an underscore
 VerNum = '';		% optional version number for testing
 
 % Choose C2P function
-par.C2Pfunctiontype = 'C';
+par.C2Pfunctiontype = 'P';
 % 'P' -> PO4 function ; 'C' -> Cell model; 'T' -> Temperature function; 'R' -> constant value (Redfield)
 % 
 GridVer  = 91  ;
@@ -81,8 +81,8 @@ par.opt_R_Si  = on ;
 par.opt_rR    = on ; 
 % --- C:P function parameters -----
 % phosphate-dependent function parameters
-par.opt_cc    = off ;
-par.opt_dd    = off ; 
+par.opt_cc    = on ;
+par.opt_dd    = on ; 
 % temperature-dependent function parameters
 par.opt_ccT   = off ; 
 par.opt_ddT   = off ;
