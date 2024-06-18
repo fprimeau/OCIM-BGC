5c163b8d0f08db37deb795dab085be64e9f4ab9d
Megan Sullivan(gp) <56322448+MeganRSullivan@users.noreply.github.com>
2024-06-18 09:09:55 -0700

    tmp: start run optPCO_Const_N23in_nosmooth

--- Code_withCellModel/driver/slurm_run_wrapper.m
+++ Code_withCellModel/driver/slurm_run_wrapper.m
@@ -2,12 +2,12 @@
 % running this script in the slurm matlab call instead of running driver.m
 % directly should suppress the command line prompts from appearing in the
 % output file
-diary logs/optPCO_Tz_N23in_nosmooth.out
+diary logs/optPCO_Const_N23in_nosmooth.out
 %fprintf('----------------------------------- \n\n')
 dbstop if error
 date_start = datetime('now');
 fprintf('Start Date: %s\n',date_start)
-fprintf('Reoptimizing P-dependent model using the SetUp input fields from the Nature paper (Wang et al., 2023) using initial guess on CO and params from from hojongs previous run.  No smoothing \n\n')
+fprintf('Reoptimizing constant C:P model using the SetUp input fields from the Nature paper (Wang et al., 2023) using initial guess on CO and params from from hojongs previous run.  No smoothing \n\n')
 run driver_SetUpv2.m
 date_end = datetime('now'); fprintf('Complete Date: %s\n',date_end)
 fprintf('Total elapsed time was %s\n',date_end - date_start)
