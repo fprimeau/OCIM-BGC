19b331d77791a6a82680074fda2022dce7bf128a
Megan Sullivan(gp) <56322448+MeganRSullivan@users.noreply.github.com>
2024-06-27 01:33:27 -0700

    tmp: edit SetUp_v2; run optPCO_GM15_N23in_test1
    Test 1: only smooth Temp obs
    
    testing why optPCO_GM15_SetUpv2_N23in_nosmooth gets different result from reoptNature.
    make SetUp_v2.m match src_reoptNature/SetUp.m. except do not smooth temperature.
    
    changes from optPCO_GM15_N23in_nosmooth:
    SetUp_v2.m:
    - include smooth DIP_obs
    - change NPP setup to use GM15 function to convert to Pnpp.
    - (leave removed smoothing from Temp obs)
    - remove exclude po4 and o2 med data.
    
    run_slurm_wrapper.m:
    - change log file name: optPCO_GM15_N23in_test1.out
    
    driver_SetUpv2.m:
    - VerName = 'optPCO_GM15_SetUpv2_N23in_test1'

--- Code_withCellModel/driver/slurm_run_wrapper.m
+++ Code_withCellModel/driver/slurm_run_wrapper.m
@@ -2,12 +2,12 @@
 % running this script in the slurm matlab call instead of running driver.m
 % directly should suppress the command line prompts from appearing in the
 % output file
-diary logs/optPCO_Tz_N23in_nosmooth.out
+diary logs/optPCO_GM15_N23in_test1.out
 %fprintf('----------------------------------- \n\n')
 dbstop if error
 date_start = datetime('now');
 fprintf('Start Date: %s\n',date_start)
-fprintf('Reoptimizing ctemperature-dependent C:P model using the SetUp input fields from the Nature paper (Wang et al., 2023) using initial guess on CO and params from from hojongs previous run.  No smoothing \n\n')
+fprintf('Reoptimizing phosphate-dependent C:P model using the SetUp input fields from the Nature paper (Wang et al., 2023) using initial guess on CO and params from from hojongs previous run.  No smoothing Temp obs; smoothed DIP_ob; gm15 satellite npp conversions \n\n')
 run driver_SetUpv2.m
 date_end = datetime('now'); fprintf('Complete Date: %s\n',date_end)
 fprintf('Total elapsed time was %s\n',date_end - date_start)
