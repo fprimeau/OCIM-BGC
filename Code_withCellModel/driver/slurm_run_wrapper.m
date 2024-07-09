46e40ef633695c2950fae99e263a6e2e0c7de413
Megan Sullivan(gp) <56322448+MeganRSullivan@users.noreply.github.com>
2024-07-08 23:45:21 -0700

    slurm_run_wrapper: add clear all; close all at start of file

--- Code_withCellModel/driver/slurm_run_wrapper.m
+++ Code_withCellModel/driver/slurm_run_wrapper.m
@@ -2,6 +2,7 @@
 % running this script in the slurm matlab call instead of running driver.m
 % directly should suppress the command line prompts from appearing in the
 % output file
+clear all; close all; clc;
 diary logs/optPCO_GM15_N23in_testNPPp2c_Nature_ccdd.out
 %fprintf('----------------------------------- \n\n')
 dbstop if error
